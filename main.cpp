facbb774fa2225bbb3aadd344a35f94688c3a610
Mightius Ignatius <ignaty8@gmail.com>
2022-03-08 21:20:22 +0100

    main.cpp: Basic functions

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+#include <queue>
+#include <string>
+#include <fstream>
+#include <map>
+#include <typeindex>
+#include <memory>
+#include <iostream>
+#include <json.hpp>
+
 enum e_TaskType {
     ADD,
     SUB,
@@ -9,14 +18,6 @@ enum e_TaskType {
 
 typedef e_TaskType TaskType;
 
-static const std::map<TaskType, std::type_index> taskTypeMap = 
-{
-    {TaskType::ADD, typeid(TaskAdd)},
-    {TaskType::SUB, typeid(TaskSub)},
-    {TaskType::MUL, typeid(TaskMul)},
-    {TaskType::DIV, typeid(TaskDiv)}
-};
-
 class TaskHandler {
 
 public:
@@ -24,6 +25,7 @@ public:
 
     //TaskType type;
     virtual int execute() = 0;
+    //virtual ~TaskHandler() = 0;
 
 protected:
     
@@ -31,38 +33,85 @@ protected:
 
 };
 
-class TaskAdd : TaskHandler {
+class TaskAdd : public TaskHandler {
 
 public:
-    virtual int execute()
+    TaskAdd(int x, int y) : TaskHandler(x,y) {};
+
+    int execute()
     {
         return x + y;
     }
 };
 
-class TaskSub : TaskHandler {
+class TaskSub : public TaskHandler {
 
 public:
+    TaskSub(int x, int y) : TaskHandler(x,y) {};
     virtual int execute()
     {
         return x - y;
     }
 };
 
-class TaskMul : TaskHandler {
+class TaskMul : public TaskHandler {
 
 public:
+    TaskMul(int x, int y) : TaskHandler(x,y) {};
     virtual int execute()
     {
         return x * y;
     }
 };
 
-class TaskDiv : TaskHandler {
+class TaskDiv : public TaskHandler {
 
 public:
+    TaskDiv(int x, int y) : TaskHandler(x,y) {};
     virtual int execute()
     {
         return x / y;
     }
 };
+
+std::unique_ptr<TaskHandler> taskHandlerCreate(std::string taskName, int x, int y)
+{
+    // TODO: While a map from string to type would work better,
+    // I cannot currently figure out how to make that work.
+    //TaskAdd taskAdd = TaskAdd(x,y);
+    
+    if (taskName == "ADD") return std::make_unique<TaskAdd>(x,y);
+    if (taskName == "SUB") return std::make_unique<TaskSub>(x,y);
+    if (taskName == "MUL") return std::make_unique<TaskMul>(x,y);
+    if (taskName == "DIV") return std::make_unique<TaskDiv>(x,y);
+    return std::unique_ptr<TaskHandler>(nullptr);
+}
+
+std::queue<std::unique_ptr<TaskHandler>> loadTasks(std::string filename)
+{
+    std::ifstream jsonFile(filename);
+    auto json = nlohmann::json::parse(jsonFile);
+    std::queue<std::unique_ptr<TaskHandler>> taskQueue;
+
+    for(const auto& taskJson: json["tasks"])
+    {
+        std::string taskTypeStr = taskJson["type"];
+        int taskX = taskJson["num1"];
+        int taskY = taskJson["num2"];
+        taskQueue.push(taskHandlerCreate(taskTypeStr, taskX, taskY));
+    }
+    
+    return taskQueue;
+}
+
+int main()
+{
+    auto taskQueue = loadTasks("tasks.json");
+    
+    while(!taskQueue.empty())
+    {
+        std::cout << taskQueue.front()->execute() << ", ";
+        taskQueue.pop();
+    }
+    return 0;
+}
