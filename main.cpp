c7e73fe5787c0e6783881e90d8ae8297b6971857
Mightius Ignatius <ignaty8@gmail.com>
2022-03-09 00:05:54 +0100

    main.cpp: Properly commented.

--- main.cpp
+++ main.cpp
@@ -6,13 +6,18 @@
 #include <stdexcept>
 #include <json.hpp>
 
+/*!
+    \class TaskHandler
+    \brief Abstract class for processing all tasks.
+*/
 class TaskHandler {
 
 public:
     TaskHandler(int x, int y, std::string name) : x(x), y(y), name(name) {}
 
-    virtual int execute() = 0;
+    virtual int execute() = 0; // Performs task function
     
+    //! \brief Overload << for pretty-printing
     friend std::ostream& operator<<(std::ostream& os, const TaskHandler& taskHandler)
     {
         os << taskHandler.name << ": " << taskHandler.x << ", " << taskHandler.y;
@@ -26,6 +31,10 @@ protected:
 
 };
 
+/*!
+    \class TaskAdd
+    \brief Adds 2 numbers together
+*/
 class TaskAdd : public TaskHandler {
 
 public:
@@ -36,6 +45,10 @@ public:
     }
 };
 
+/*!
+    \class TaskSub
+    \brief Substracts one number from another
+*/
 class TaskSub : public TaskHandler {
 
 public:
@@ -46,6 +59,10 @@ public:
     }
 };
 
+/*!
+    \class TaskMul
+    \brief Multiplies 2 numbers together
+*/
 class TaskMul : public TaskHandler {
 
 public:
@@ -56,6 +73,10 @@ public:
     }
 };
 
+/*!
+    \class TaskDiv
+    \brief Divides one number by another. Throws exception if threatened.
+*/
 class TaskDiv : public TaskHandler {
 
 public:
@@ -68,6 +89,7 @@ public:
     }
 };
 
+//! \brief Creates TaskHandler instance based on task name. Returns a nullptr if task type unkown.
 std::unique_ptr<TaskHandler> taskHandlerCreate(std::string taskName, int x, int y)
 {
     // TODO: While a map from string to type would work better,
@@ -80,27 +102,31 @@ std::unique_ptr<TaskHandler> taskHandlerCreate(std::string taskName, int x, int 
     return std::unique_ptr<TaskHandler>(nullptr);
 }
 
+//! \brief Loads all tasks from JSON file. Returns queue of TaskHandler instances.
 std::queue<std::unique_ptr<TaskHandler>> loadTasks(std::string filename)
 {
     try
     {
+        // Parse JSON file
         std::ifstream jsonFile(filename);
         auto json = nlohmann::json::parse(jsonFile);
-        std::queue<std::unique_ptr<TaskHandler>> taskQueue;
+        std::queue<std::unique_ptr<TaskHandler>> taskQueue; // Queue for task handlers
+
         for(const auto& taskJson: json["tasks"])
         {
+            // Parse every entry
             std::string taskTypeStr = taskJson["type"];
             int taskX = taskJson["num1"];
             int taskY = taskJson["num2"];
             std::unique_ptr<TaskHandler> taskHandler = taskHandlerCreate(taskTypeStr, taskX, taskY);
-            if (taskHandler == nullptr)
+            if (taskHandler == nullptr) // Check if task is valid
                 throw std::invalid_argument(std::string("Invalid task definition in: ") + std::string(taskJson.dump()));
-            taskQueue.push(std::move(taskHandler));
+            taskQueue.push(std::move(taskHandler)); // Push to queue, move on
         }    
           
         return taskQueue;
     }
-    catch(const std::exception& e)
+    catch(const std::exception& e) // Catch JSON Parsing and other errors
     {
         std::cerr << "Error processing input JSON file: \"" << filename << "\"\n" << e.what() << '\n';
         exit(1);
@@ -109,25 +135,29 @@ std::queue<std::unique_ptr<TaskHandler>> loadTasks(std::string filename)
 
 int main(int argc, char* argv[])
 {
+    // Go over every input file
     for(int k = 1; k < argc; k++)
     {
         auto taskQueue = loadTasks(argv[k]);
         
+        // Process task queue
         while(!taskQueue.empty())
         {
             int result;
             try
             {
+                // Process task
                 result = taskQueue.front()->execute();
                 std::cout << result;
             }
-            catch(const std::exception& e)
+            catch(const std::exception& e) // Catch task execution errors
             {
                 std::cerr << "Exception while processing task \"" << *(taskQueue.front().get()) << "\"\n" << e.what();
             }
             
             taskQueue.pop();
             
+            // Check to avoid trailing coma
             if(!taskQueue.empty())
                 std::cout << ", ";
         }
